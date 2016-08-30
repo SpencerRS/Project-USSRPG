35d277bbad194c8cc7e70439b39b287f4668dee4
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-08-30 15:49:05 -0700

    Imported  (some) previous work from Rough Draft. Buttons and other activities.

--- Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/MainActivity.java
+++ Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/MainActivity.java
@@ -1,13 +1,70 @@
 package com.spizzyrichlife.ussrpg_v01;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button toLobby;
+    Button toPool;
+    Button toCreation;
+    Button toSelection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //HOWTO: Make a button transition to another Activity
+//        X = (Button) findViewById(R.id.X);
+//        toCreation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, X.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        //TODOne: Lobby Button to Multi-player Lobby Activity
+        toLobby = (Button) findViewById(R.id.lobbyMainButton);
+        toCreation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MpLobbyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //TODOne: Pool Button
+        toPool = (Button) findViewById(R.id.poolMainButton);
+        toCreation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DicePoolerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //TODOne: Creation Button link to Characer Creation Activity
+        toCreation = (Button) findViewById(R.id.createMainButton);
+        toCreation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CharacterCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //TODOne: Selection Button to Character Selection Activity
+        toSelection = (Button) findViewById(R.id.selectMainButton);
+        toSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CharacterSelectionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/MpLobbyActivity.java
@@ -0,0 +1,13 @@
+package com.spizzyrichlife.ussrpg_v01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MpLobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mp_lobby);
+    }
+}
