178a112421f4155a8e0b40c9a333e7c48fe7b957
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-08-30 15:03:35 -0700

    Added SQL Database Helper space and HOW TO

---
+++ Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/DBHelper.java
@@ -0,0 +1,26 @@
+package com.spizzyrichlife.ussrpg_v01;
+
+/**
+ * Created by SpizzyRich on 8/30/16.
+ */
+
+//HOW TO: Set up a DB helper
+//private static final int DATABASE_VERSION = 1;
+//public static final String DATABASE_NAME = "TEST_DB";
+//public static final String EXAMPLE_LIST_TABLE_NAME = "EXAMPLE_LIST";
+//
+//public static final String COL_ID = "_id";
+//public static final String COL_ITEM_NAME = "ITEM_NAME";
+//public static final String COL_ITEM_DESCRIPTION = "DESCRIPTION";
+//
+//public static final String[] EXAMPLE_COLUMNS = {COL_ID,COL_ITEM_NAME,COL_ITEM_DESCRIPTION};
+//
+//private static final String CREATE_EXAMPLE_LIST_TABLE =
+//        "CREATE TABLE " + EXAMPLE_LIST_TABLE_NAME +
+//        "(" +
+//        COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+//        COL_ITEM_NAME + " TEXT, " +
+//        COL_ITEM_DESCRIPTION + " TEXT )";
+
+public class DBHelper {
+}
