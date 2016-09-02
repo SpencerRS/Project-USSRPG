06a5b2799ededba8db4369260dab02990fedded5
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-09-01 18:44:18 -0700

    Includes >4 user stories in ready

--- Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/MainActivity.java
+++ Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/MainActivity.java
@@ -10,12 +10,12 @@ import android.widget.Button;
 //Your work must:
 //   []   Have a theme and display data related to that theme.
 //   []   Allow the user to search by at least three different search criterias
-//   []   Show results in separate entries/pages to the user, including descriptions, images, and whatever else you like
-//   []   Gather data for the search results from the database
+//   []     Show results in separate entries/pages to the user, including descriptions, images, and whatever else you like
+//   []     Gather data for the search results from the database
 //   []   Allow the user to add rows to a database, which they can view at any time
 //   []   Incorporate Object Oriented principles into your app
 //   []   Contain code comments, explaining the functionality behind sections of code
-//   []   Include at least 4 user stories in the readme.md
+//   [X]  Include at least 4 user stories in the readme.md
 //   []   Include espresso tests for your app
 //   []   Ensure that you add a sufficient number of tests for the features of each screen of your app
 //Bonus:
