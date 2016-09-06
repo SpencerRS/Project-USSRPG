47ecc7681b36b66f474b56d8f8d600491814113e
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-09-06 16:10:33 -0700

    restarting comp

---
+++ Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/StethoMyApplication.java
@@ -0,0 +1,34 @@
+package com.spizzyrichlife.ussrpg_v01;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by SpizzyRich on 9/6/16.
+ */
+public class StethoMyApplication extends Application {
+    @Override
+    public void onCreate() {
+// Create an InitializerBuilder
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(this);
+
+// Enable Chrome DevTools
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+        );
+
+//// Enable command line interface
+//        initializerBuilder.enableDumpapp(
+//                Stetho.defaultDumperPluginsProvider(this)
+//        );
+
+// Use the InitializerBuilder to generate an Initializer
+        Stetho.Initializer initializer = initializerBuilder.build();
+
+// Initialize Stetho with the Initializer
+        Stetho.initialize(initializer);
+        super.onCreate();
+    }
+}
