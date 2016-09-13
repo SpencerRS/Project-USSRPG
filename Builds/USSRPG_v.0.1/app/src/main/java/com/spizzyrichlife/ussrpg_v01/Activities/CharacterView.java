0bf171fa2017c07f7bf86f490cc1a0d4e456d402
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-09-12 17:42:43 -0700

    Adds Char. View XML

---
+++ Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/Activities/CharacterView.java
@@ -0,0 +1,15 @@
+package com.spizzyrichlife.ussrpg_v01.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.spizzyrichlife.ussrpg_v01.R;
+
+public class CharacterView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_view);
+    }
+}
