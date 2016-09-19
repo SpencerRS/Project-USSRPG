65a758a8cd9da0daa8db57210e293802d5967e78
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-09-19 16:55:42 -0700

    Dice values stored as individuals (WET/verbose version.)

--- Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/Activities/DiceRoller.java
+++ Builds/USSRPG_v.0.1/app/src/main/java/com/spizzyrichlife/ussrpg_v01/Activities/DiceRoller.java
@@ -14,9 +14,27 @@ public class DiceRoller {
 
     //Set Green Dice Values (d8)
     String[] green1 = {"blank",                 "0",    "0"}; //[0] = description, [1] = success, [2] = advantage
+    String[] green2 = {"successes",             "2",    "0"};
+    String[] green3 = {"advantage",             "0",    "1"};
+    String[] green4 = {"success",               "1",    "0"};
+    String[] green5 = {"success",               "1",    "0"};
+    String[] green6 = {"success",               "1",    "0"};
+    String[] green7 = {"advantage",             "0",    "1"};
+    String[] green8 = {"success + advantage",   "1",    "1"};
 
     //Set Yellow Dice Values (d12)
     String[] yellow1 = {"blank",                "0",    "0"}; //[0] = description, [1] = success, [2] = advantage
+    String[] yellow2 = {"successes",            "2",    "0"};
+    String[] yellow3 = {"advantage",            "0",    "1"};
+    String[] yellow4 = {"success",              "1",    "0"};
+    String[] yellow5 = {"success + advantage",  "1",    "1"};
+    String[] yellow6 = {"advantages",           "0",    "2"};
+    String[] yellow7 = {"advantages",           "0",    "2"};
+    String[] yellow8 = {"success + advantage",  "1",    "1"};
+    String[] yellow9 = {"success",              "1",    "0"};
+    String[] yellow10 = {"success + advantage", "1",    "1"};
+    String[] yellow11 = {"successes",           "2",    "0"};
+    String[] yellow11 = {"critical success",    "3",    "3"};
 
     //Set Black Dice Values (d6)
     String[] black1 = {"blank",                 "0",    "0"}; //[0] = description, [1] = success, [2] = advantage
@@ -28,7 +46,25 @@ public class DiceRoller {
 
     //Set Purple Dice Values (d8)
     String[] purple1 = {"blank",                "0",    "0"}; //[0] = description, [1] = success, [2] = advantage
+    String[] purple2 = {"disadvantage",         "0",    "-1"};
+    String[] purple3 = {"fail + disadvantage",  "-1",   "-1"};
+    String[] purple4 = {"fails",                "-2",   "0"};
+    String[] purple5 = {"disadvantage",         "0",    "-1"};
+    String[] purple6 = {"disadvantage",         "0",    "-1"};
+    String[] purple7 = {"fails",                "-2",   "0"};
+    String[] purple8 = {"fail",                 "-1",   "0"};
 
     //Set Red Dice Values (d12)
     String[] red1 = {"blank",                   "0",    "0"}; //[0] = description, [1] = success, [2] = advantage
+    String[] red2 = {"disadvantage",            "0",    "-1"};
+    String[] red3 = {"fails",                   "-2",   "0"};
+    String[] red4 = {"fail",                    "-1",   "0"};
+    String[] red5 = {"fail + disadvantage",     "-1",   "-1"};
+    String[] red6 = {"disadvantages",           "0",    "-2"};
+    String[] red7 = {"disadvantages",           "0",    "-2"};
+    String[] red8 = {"fail + disadvantage",     "-1",   "-1"};
+    String[] red9 = {"fail",                    "-1",   "0"};
+    String[] red10 = {"fails",                  "-2",   "0"};
+    String[] red11 = {"disadvantage",           "0",    "-1"};
+    String[] red12 = {"critical fail",          "-3",   "-3"};
 }
